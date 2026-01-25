3f7e2ee27f79db1c17c75b95fd2be9ada5270688
lakshya <ahlawat.lakshya.2004@gmail.com>
2026-01-25 22:19:45 +0530

    first commit

--- src/config.js
+++
@@ -1,6 +0,0 @@
-export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
-export const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-export const LINKEDIN_URL = process.env.REACT_APP_LINKEDIN_URL;
-export const TWITTER_URL = process.env.REACT_APP_TWITTER_URL;
-export const LEETCODE_URL = process.env.REACT_APP_LEETCODE_URL;
-export const GFG_URL = process.env.REACT_APP_GFG_URL;
